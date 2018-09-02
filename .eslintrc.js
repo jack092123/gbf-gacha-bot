b04588e97466a15a34e6c46540082914666cb00a
RoachOwO <jack092123@gmail.com>
2018-09-02 11:09:30 +0800

    Project init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "es6": true,
+    },
+    "extends": "airbnb-base",
+    "rules": {
+        "linebreak-style": ["error", "windows"],
+        "quotes": ["error", "double"],
+        "no-console": 0,
+    },
+};
